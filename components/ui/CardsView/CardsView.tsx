f0d3e2b2bcf2b3bde05ea3846836fd33f1acdd1a
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2022-08-12 08:00:42 -0300

    s

--- components/ui/CardsView/CardsView.tsx
+++ components/ui/CardsView/CardsView.tsx
@@ -6,23 +6,27 @@ import { ContentMediaCard } from '../../../components/ui/ContentMediaCard/Conten
 import LanguageIcon from '@mui/icons-material/Language';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import { Box } from '@mui/material';
+import { useRouter } from 'next/router';
 const CardsView = () => {
+    const { asPath } = useRouter()
     return (
         <>
-        <Box display='flex' justifyContent='center' sx={{flexWrap:'wrap'}}>
-        <div data-aos="zoom-in-left">
-            <ContentMediaCard title='@HatorClothing' icon={<InstagramIcon sx={{ fontSize: 50, color: '#846267', textShadow: '1px 1px 1px #bf4b4b' }} />} url='#' button='Seguinos!' />
-        </div>
-        <div data-aos="zoom-in-right">
-            <ContentMediaCard title='Solicita tu catálogo' icon={<WhatsAppIcon sx={{ fontSize: 50, color: '#846267', textShadow: '1px 1px 1px #bf4b4b' }} />} url='#' button='Consultanos!' />
-        </div>
-        <div data-aos="zoom-in-left">
-            <ContentMediaCard title='TikTok' icon={<MusicNoteIcon sx={{ fontSize: 50, color: 'black', textShadow: '1px 1px 1px #bf4b4b' }} />} url='#' button='Seguinos!' />
-        </div>
-        <div data-aos="zoom-in-right">
-            <ContentMediaCard title='Nuestra web' icon={<LanguageIcon sx={{ fontSize: 50, color: '#846267', textShadow: '1px 1px 1px #bf4b4b' }} />} url='/' button='visitanos!' />
-        </div>
-        </Box>
+            <Box display='flex' justifyContent='center' sx={{ flexWrap: 'wrap' }}>
+                <div data-aos="zoom-in-left">
+                    <ContentMediaCard title='@HatorClothing' icon={<InstagramIcon sx={{ fontSize: 50, color: '#846267', textShadow: '1px 1px 1px #bf4b4b' }} />} url='#' button='Seguinos!' />
+                </div>
+                <div data-aos="zoom-in-right">
+                    <ContentMediaCard title='Solicita tu catálogo' icon={<WhatsAppIcon sx={{ fontSize: 50, color: '#846267', textShadow: '1px 1px 1px #bf4b4b' }} />} url='#' button='Consultanos!' />
+                </div>
+                <div data-aos="zoom-in-left">
+                    <ContentMediaCard title='TikTok' icon={<MusicNoteIcon sx={{ fontSize: 50, color: 'black', textShadow: '1px 1px 1px #bf4b4b' }} />} url='#' button='Seguinos!' />
+                </div>
+                <Box sx={{ display: asPath == '/' ? 'none' : '' }}>
+                    <div data-aos="zoom-in-right">
+                        <ContentMediaCard title='Nuestra web' icon={<LanguageIcon sx={{ fontSize: 50, color: '#846267', textShadow: '1px 1px 1px #bf4b4b' }} />} url='/' button='visitanos!' />
+                    </div>
+                </Box>
+            </Box>
         </>
     )
 }
