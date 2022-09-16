274814003e88bcb38ed9a85067c9e7977b79c872
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2022-09-16 12:07:14 -0300

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,48 +4,21 @@ import { Layout } from '../components'
 import CardsView from '../components/ui/CardsView/CardsView';
 import { db } from '../database';
 import { useEffect, useState } from 'react';
-import { Wheel } from 'react-custom-roulette'
-
-const data = [
-  { option: '10% OFF', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: 'La proxima quizá!' },
-  { option: '5% OFF', style: { backgroundColor: 'black', textColor: 'white' } },
-  { option: 'La proxima quizá!' },
-]
-
 
 
 const Home: NextPage = () => {
-
-
-  useEffect(() => {
-    console.log("window.innerHeight", window.innerHeight);
-
-
-  }, [])
-  const [start, setStart] = useState(false);
-  const [prizeNumber, setPrizeNumber] = useState(0);
   return (
     <>
       <Layout title='Hator - Cloting'>
-        <div data-aos="zoom-in-down">
-          <Box display='flex' justifyContent='center'>
-            <Wheel
-              mustStartSpinning={start}
-              prizeNumber={3}
-              data={data}
-              backgroundColors={['#3e3e3e', '#df3428']}
-              textColors={['#ffffff']}
-            />
-          </Box>
-        </div>
-        <Button onClick={() => setStart(true)}>spin</Button>
+
       </Layout>
     </>
   )
 }
 
 export default Home
+
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const movies = await db.connect()
   return {
